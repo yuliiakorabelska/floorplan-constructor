a0e80590d1705c6fdd47a51d266b2734e734ef3c
Yuliia Korabelska <yuliia@tonic.ai>
2023-08-23 22:22:27 -0400

    init

---
+++ floor-plan-js/src/Canvas/Canvas.jsx
@@ -0,0 +1,11 @@
+
+import React, { useRef } from 'react'
+
+const Canvas = props => {
+  
+  const canvasRef = useRef(null)
+  
+  return <canvas ref={canvasRef} {...props}/>
+}
+
+export default Canvas
